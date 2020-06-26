4070474e940a3599bebfc567c8c9e1b0747ec7c9
shin donggyu <donggyu9410@gmail.com>
2020-06-27 01:36:41 +0900

    Add comments in sentence_reversal

--- sentence_reversal.py
+++ sentence_reversal.py
@@ -5,6 +5,20 @@ sentence reversal
 "World Hello" 이런식으로 단어는 바뀌지 않고 단어의 순서가 바뀌는 알고리즘
 """
 
+from typing import List
+
+
+def rev_word1(s: str) -> str:
+    return " ".join(reversed(s.split()))
+
+
+"""
+Solution
+
+단어들만 담는 리스트를 하나 만들어서, 
+해당 리스트를 역순으로 출력해준다. 
+"""
+
 
 def rev_word(s: str) -> str:
 
@@ -31,6 +45,11 @@ def rev_word(s: str) -> str:
         i += 1
 
     return " ".join(reversed(words))
+
+
+def rev_words(words: List[str]) -> List[str]:
+    # words 리스트를 역순으로 반환해주는 함수를 직접 작성해보세요
+    return []
 
 
 print(rev_word("Hello World"))
